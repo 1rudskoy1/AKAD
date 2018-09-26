1c0ce18061bec600b619f94670ac84aab7ef0e6e
Ganiev RUSTAM <ganiev.rystam@gmail.com>
2018-09-26 16:56:37 +0300

    this project completed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
 var TabsIndex = 1;
+var categories = document.querySelectorAll('.Posts__categories_category');
+var tags = document.querySelectorAll('.Tags__all-tags_tag');
+for (let i = 0; i< categories.length;i++) {
+
+	categories[i].onclick = function (e){
+		var target = e.target;
+		for (let i = 0; i < categories.length; i++) {
+				categories[i].className = 'Posts__categories_category';
+				target.className +=  " category-active";
+			}
+		
+		}
+	}
+	for (let i = 0; i< tags.length;i++) {
+
+		tags[i].onclick = function (e){
+			var target = e.target;
+			for (let i = 0; i < tags.length; i++) {
+				tags[i].className = 'Tags__all-tags_tag';
+				target.className +=  " tags-active";
+			}
+		
+		}
+	}
+
 
 showTabs(TabsIndex);
 
@@ -18,3 +43,6 @@ function showTabs(n){
 	content[TabsIndex - 1].style.display = "block";
 	dots[TabsIndex-1].style.background = "#19bd9a";
 }
+
+
+
